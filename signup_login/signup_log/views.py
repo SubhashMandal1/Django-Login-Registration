07a31c09887fb477d50edb261e82059d2c465c16
subhashmandal1 <subhashjarvis@gmail.com>
2024-05-01 17:07:35 +0545

    Changed views for authentication

--- signup_login/signup_log/views.py
+++ signup_login/signup_log/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import UserCreds
+from django.http import JsonResponse 
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
+    if request.method == 'POST':
+        mail = request.POST['email']
+        user = request.POST['username']
+        passw = request.POST['password']
+        confirm = request.POST['conpass']
+        
+        new_user= UserCreds(email=mail, username=user, password=passw)
+        new_user.save()
     return render(request,'index.html')
+
+
+def check_username(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        print(username)
+        exists = UserCreds.objects.filter(username=username).exists()
+        return JsonResponse({'exists': exists})
